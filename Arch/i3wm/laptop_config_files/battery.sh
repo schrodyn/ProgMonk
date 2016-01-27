75c6608117c05c6c6a112a640935d1bf62c37eaf
Flynston <shalyrom@mail.ru>
2016-01-27 10:42:32 +0300

    charge to energy prefix changed

--- Arch/i3wm/laptop_config_files/battery.sh
+++ Arch/i3wm/laptop_config_files/battery.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-charge_curr=$(cat /sys/class/power_supply/BAT1/charge_now)
-charge_full=$(cat  /sys/class/power_supply/BAT1/charge_full)
+energy_curr=$(cat /sys/class/power_supply/BAT1/energy_now)
+energy_full=$(cat  /sys/class/power_supply/BAT1/energy_full)
 
-procent=$(echo "$charge_curr $charge_full" | awk '{printf "%.2f", $1*100/$2 }')
+procent=$(echo "$energy_curr $energy_full" | awk '{printf "%.2f", $1*100/$2 }')
 
-power_supply_current_now=$(cat /sys/class/power_supply/BAT1/current_now)
-power_supply_charge_now=$(cat /sys/class/power_supply/BAT1/charge_now)
+power_supply_current_now=$(cat /sys/class/power_supply/BAT1/power_now)
+power_supply_energy_now=$(cat /sys/class/power_supply/BAT1/energy_now)
 
-minutes=$(echo "$power_supply_current_now $power_supply_charge_now" | awk '{printf "%d", $2*60/$1}')
+minutes=$(echo "$power_supply_current_now $power_supply_energy_now" | awk '{printf "%d", $2*60/$1}')
 
 rem_time=$(echo $minutes | awk '{ printf "%d:%02d", $1 / 60, $1 % 60}')
 
