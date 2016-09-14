656cadd74ea51fbd39b0899da94dfbbe671dcef9
Flynston <roman.shalymov@gmail.com>
2016-09-14 17:51:03 +0300

    Vim setup script added

---
+++ Arch/i3wm/laptop_config_files/vim_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "setup vundle..."
+ 
+mkdir -p $HOME/.vim/plugins
+VUNDLE_DIR="$HOME/.vim/plugins/Vundle.vim"
+rm -rf $VUNDLE_DIR
+git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+
+echo "download vimrc config..."
+curl https://raw.githubusercontent.com/Flynston/ProgMonk/master/Arch/i3wm/laptop_config_files/vimrc > $HOME/.vimrc
+
+echo "setup color scheme config..."
+mkdir -p $HOME/.vim/colors
+curl https://raw.githubusercontent.com/Flynston/ProgMonk/master/Arch/i3wm/laptop_config_files/vim/colors/vendetta.vim > $HOME/.vim/colors/vendetta.vim
